3c79c38c5a8473afe65f8a26c69d8f998a29bdeb
gschivley <gschivley@users.noreply.github.com>
2017-12-13 13:35:15 -0500

    Advance version to 0.1.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ghgforcing',
-      version='0.1.6',
+      version='0.1.7',
       description='Calculate radiative forcing from GHG emissions',
       url='https://github.com/gschivley/ghgforcing',
       author='Greg Schivley',
